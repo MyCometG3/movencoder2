50e86c7e334d26b136884e63d15b4a3a5f0bf7fd
Takashi Mochizuki <mycometg3@icloud.com>
2023-11-02 08:45:21 +0900

    It's 2023.

--- movencoder2/parseUtil.m
+++ movencoder2/parseUtil.m
@@ -3,7 +3,7 @@
 //  movencoder2
 //
 //  Created by Takashi Mochizuki on 2019/06/16.
-//  Copyright © 2019-2021 MyCometG3. All rights reserved.
+//  Copyright © 2019-2023 MyCometG3. All rights reserved.
 //
 
 /*
