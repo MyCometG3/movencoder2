ced01995c7e17def52e3add37e76010cbb78c97d
Takashi Mochizuki <mycometg3@icloud.com>
2025-09-06 18:22:18 +0900

    refactor: move static arrays to implementation, declare extern in header

--- movencoder2/MECommon.h
+++ movencoder2/MECommon.h
@@ -43,28 +43,10 @@
 /* =================================================================================== */
 
 // Standard MPEG source channel layouts (8 channels max)
-static const AudioChannelLayoutTag kMEMPEGSourceLayouts[8] = {
-    kAudioChannelLayoutTag_Mono,        // C
-    kAudioChannelLayoutTag_Stereo,      // L R
-    kAudioChannelLayoutTag_MPEG_3_0_A,  // L R C
-    kAudioChannelLayoutTag_MPEG_4_0_A,  // L R C Cs
-    kAudioChannelLayoutTag_MPEG_5_0_A,  // L R C Ls Rs
-    kAudioChannelLayoutTag_MPEG_5_1_A,  // L R C LFE Ls Rs
-    kAudioChannelLayoutTag_MPEG_6_1_A,  // L R C LFE Ls Rs Cs
-    kAudioChannelLayoutTag_MPEG_7_1_C,  // L R C LFE Ls Rs Rls Rrs
-};
+extern const AudioChannelLayoutTag kMEMPEGSourceLayouts[8];
 
 // Standard AAC destination channel layouts (8 channels max)
-static const AudioChannelLayoutTag kMEAACDestinationLayouts[8] = {
-    kAudioChannelLayoutTag_Mono,        // C
-    kAudioChannelLayoutTag_Stereo,      // L R
-    kAudioChannelLayoutTag_AAC_3_0,     // C L R
-    kAudioChannelLayoutTag_AAC_4_0,     // C L R Cs
-    kAudioChannelLayoutTag_AAC_5_0,     // C L R Ls Rs
-    kAudioChannelLayoutTag_AAC_5_1,     // C L R Ls Rs Lfe
-    kAudioChannelLayoutTag_AAC_6_1,     // C L R Ls Rs Cs Lfe
-    kAudioChannelLayoutTag_AAC_7_1_B    // C L R Ls Rs Rls Rrs LFE
-};
+extern const AudioChannelLayoutTag kMEAACDestinationLayouts[8];
 
 /* =================================================================================== */
 // MARK: - Progress Callback Keys
