56decf42318b8c5fd79bb82101e8be636e9ff3d1
Takashi Mochizuki <mycometg3@icloud.com>
2025-09-07 23:11:22 +0900

    It's 2025.

--- movencoder2/MEOutput.m
+++ movencoder2/MEOutput.m
@@ -3,7 +3,7 @@
 //  movencoder2
 //
 //  Created by Takashi Mochizuki on 2018/11/24.
-//  Copyright © 2018-2023 MyCometG3. All rights reserved.
+//  Copyright © 2018-2025 MyCometG3. All rights reserved.
 //
 
 /*
