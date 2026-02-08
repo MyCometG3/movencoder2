6c7c0488a58ee0f6b4ecf7ac57e21a2f843c57fd
Takashi Mochizuki <mycometg3@icloud.com>
2026-02-08 18:18:51 +0900

    chore: add GPL headers to transcode configuration

--- movencoder2/Core/METranscodeConfiguration.m
+++ movencoder2/Core/METranscodeConfiguration.m
@@ -3,7 +3,26 @@
 //  movencoder2
 //
 //  Created by OpenCode.
+//  Copyright © 2026 MyCometG3. All rights reserved.
 //
+
+/*
+ * This file is part of movencoder2.
+ *
+ * movencoder2 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * movencoder2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with movencoder2; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #import "METranscodeConfiguration.h"
 
